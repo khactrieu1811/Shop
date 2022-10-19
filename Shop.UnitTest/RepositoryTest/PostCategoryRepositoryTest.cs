02cb1d60ec011b19336971d0e163f2c8b5ebb46b
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2022-10-19 09:41:57 +0700

    Test 10

---
+++ Shop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.Data.Infrastructure;
+using Shop.Data.Repositories;
+using Shop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            Assert.AreEqual(7, list.Count);
+        }
+       
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+            //Assert để gọi phương thức so sánh
+            //
+            Assert.IsNotNull(result);
+            Assert.AreEqual(7, result.ID);
+        }
+    }
+}
