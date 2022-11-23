3237d3c4715ce1acd5a70358381dcfa33b7eecae
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2022-11-23 16:21:08 +0700

    scope vs rootscope

---
+++ Shop.Web/Scripts/spa/app.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+//khởi tạo module
+var myApp = angular.module('myModule', []);
+
+// khởi tạo controller
+// kết nổi controller vs module
+myApp.controller("schoolController", schoolController);
+myApp.controller("studentController", studentController);
+myApp.controller("teacherController", teacherController);
+
+function schoolController($scope) {
+    $scope.message = "Annoucement from school";
+}
+//declare
+function studentController($scope) {
+    $scope.message = "This is my message from student";
+}
+function teacherController($scope) {
+    $scope.message = "This is my message from teacher";
+}
