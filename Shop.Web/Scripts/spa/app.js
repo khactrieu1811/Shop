30112f60999bdbda4458cc3b4f3759c7bb218a7d
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2022-11-24 10:44:51 +0700

    Bài 19

--- Shop.Web/Scripts/spa/app.js
+++ Shop.Web/Scripts/spa/app.js
@@ -5,16 +5,26 @@ var myApp = angular.module('myModule', []);
 // khởi tạo controller
 // kết nổi controller vs module
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service("Validator", Validator);
 
-function schoolController($scope) {
-    $scope.message = "Annoucement from school";
+schoolController.$inject = ['$scope', 'Validator']
+
+function schoolController($scope, Validator) {
+
+    $scope.checkNumber = function() {
+        $scope.message = Validator.checkNumber(2);
+    }
+    $scope.num = 1;
 }
-//declare
-function studentController($scope) {
-    $scope.message = "This is my message from student";
-}
-function teacherController($scope) {
-    $scope.message = "This is my message from teacher";
+
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2) {
+            return 'this is even';
+        } else
+            return 'this is odd';
+    }
 }
