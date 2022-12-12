dbe148df47211fdd5118a822cd0275026f28c853
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2022-12-12 13:37:02 +0700

    24 add filter status

---
+++ Shop.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'kích họat';
+            else
+                return 'khóa';
+        }
+    });
+})(angular.module('tedushop.common'));
