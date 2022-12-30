532ad5f863827d00cb3c8461ba8ed014e9665df6
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2022-12-30 15:58:50 +0700

    phan trang bug

---
+++ Shop.Web/Infrastructure/Core/PaginationSet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Web.Infrastructure.Core
+{
+    // phân trang B1: tạo class generic chứa tgo6ng tin trả về
+    public class PaginationSet<T>
+    {
+        public int Page { set; get; }
+        public int Count
+        {
+            get
+            {
+                return (Items != null) ? Items.Count() : 0;
+            }
+        }
+        public int TotalPages { set; get; }
+        public int TotalCount { set; get; }
+        public IEnumerable<T> Items { set; get; }
+    }
+}
--- Shop.Web/app/components/product_categories/productCategoryListController.js
+++ Shop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,22 +1,34 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject=['$scope','apiService']
+    productCategoryListController.$inject = ['$scope', 'apiService']
+
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
+        $scope.page = 0;
+        $scope.pagesCount = 0;
+        $scope.getProductCagories = getProductCagories;
 
         // khai báo phương thức để láy dử liệu từ sever
-        $scope.getProductCategories = getProductCategories;
-
-        function getProductCategories() {
+        function getProductCagories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 20
+                }
+            }
             // 2 hàm 1 failed và success
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load productcategories failed')
             });
         }
         // gòi hàm getcategory sẽ chạy nếu controller khởi chạy
-            $scope.getProductCategories();   
+        $scope.getProductCagories();
     }
 })(angular.module('tedushop.product_categories'));
