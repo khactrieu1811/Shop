0c9bdd6ae5d3c85307eb0d3808ffabc394c2b73c
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2023-02-16 15:54:59 +0700

    Bai29 updateProductCategory

--- Shop.Web/Api/ProductCategoryController.cs
+++ Shop.Web/Api/ProductCategoryController.cs
@@ -17,13 +17,15 @@ namespace Shop.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
+        #region Initialize
+        private IProductCategoryService _productCategoryService;
 
-        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService) 
+        public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
             : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+        #endregion
         [Route("getallparents")]
         [HttpGet]
         //phân trang B2: Ap dụng vào phân trang cho webapi sử dụng linQ fwork
@@ -33,6 +35,18 @@ namespace Shop.Web.Api
             {
                 var model = _productCategoryService.GetAll();
                 var reponseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+                var reponse = request.CreateResponse(HttpStatusCode.OK, reponseData);
+                return reponse;
+            });
+        }
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+                var reponseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
                 var reponse = request.CreateResponse(HttpStatusCode.OK, reponseData);
                 return reponse;
             });
@@ -75,9 +89,35 @@ namespace Shop.Web.Api
                 {
                     var newProductCategory = new ProductCategory();
                     newProductCategory.UpdateProductCategory(producategoryVm);
+                    newProductCategory.CreatedDate = DateTime.Now;
                     _productCategoryService.Add(newProductCategory);
                     _productCategoryService.Save();
                     var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(newProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+                return response;
+
+            });
+        }
+        [Route("update")]
+        [HttpPut]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductCategoryViewModel producategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(producategoryVm.ID);
+                    dbProductCategory.UpdateProductCategory(producategoryVm);
+                    dbProductCategory.UpdatedDate = DateTime.Now;
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
                     response = request.CreateResponse(HttpStatusCode.Created, responseData);
                 }
                 return response;
--- Shop.Web/app/shared/services/apiService.js
+++ Shop.Web/app/shared/services/apiService.js
@@ -9,7 +9,8 @@
     function apiService($http, notificationService) {
         return {
             get: get,
-            post : post
+            post: post,
+            put: put
         }
         function post(url, data, success, failure) {
             $http.post(url, data).then(function (result) {
@@ -18,6 +19,19 @@
                 console.log(error.status)
                 if (error.status === '401') {
                     notificationService.displayError('Authentication is requaired');
+                }
+                else if (failure != null) {
+                    failure(error);
+                }
+            });
+        }
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
                 }
                 else if (failure != null) {
                     failure(error);
