900404b46d7bebd97ec34c0634cce6b3b69cf5e4
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2023-02-18 17:51:04 +0700

    bai 30 validate

--- Shop.Web/app/components/product_categories/productCategoryAddController.js
+++ Shop.Web/app/components/product_categories/productCategoryAddController.js
@@ -5,7 +5,6 @@
         $scope.productCategory = {
             CreatedDate: new Date(),
             Status: true,
-            Name : "Danh mục 1"
         }
         $scope.AddProductCategory = AddProductCategory;
 
