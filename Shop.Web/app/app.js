6040d2d875f15c8c591b5119e7579b1eb2a26739
DESKTOP-ULK3B69\Khac Trieu <khactrieu0147@gmail.com>
2023-03-11 14:43:17 +0700

    Bai38, 40 : tao layout cho người dùng và chưa hoàn thiện đăng nhập

--- Shop.Web/app/app.js
+++ Shop.Web/app/app.js
@@ -4,7 +4,9 @@
     angular.module('tedushop', ['tedushop.products',
                                 'tedushop.product_categories',
                                 'tedushop.common'])
-                                .config(config);
+        .config(config)
+        .config(configAuthentication);
+
 
     config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -15,11 +17,11 @@
                 url: '',
                 templateUrl: '/app/shared/views/baseView.html',
                 abstract: true
-            }).state('login', {
-                url: "/login",
-                templateUrl: "/app/components/login/loginView.html",
-                controller: "loginController"
-            })
+            })//.state('login', {
+            //    url: "/login",
+            //    templateUrl: "/app/components/login/loginView.html",
+            //    controller: "loginController"
+            //})
             .state('home', {
                 url: "/admin",
                 parent: 'base',
@@ -27,6 +29,34 @@
                 controller: "homeController"
             });
         //nếu không phải trướng hợp nào thì trả về admin
-        $urlRouterProvider.otherwise('/login');
+        $urlRouterProvider.otherwise('/admin');
+    }
+    function configAuthentication($httpProvider) {
+        $httpProvider.interceptors.push(function ($q, $location) {
+            return {
+                request: function (config) {
+
+                    return config;
+                },
+                requestError: function (rejection) {
+
+                    return $q.reject(rejection);
+                },
+                response: function (response) {
+                    if (response.status == "401") {
+                        $location.path('/login');
+                    }
+                    //the same response/modified/or a new one need to be returned.
+                    return response;
+                },
+                responseError: function (rejection) {
+
+                    if (rejection.status == "401") {
+                        $location.path('/login');
+                    }
+                    return $q.reject(rejection);
+                }
+            };
+        });
     }
 })();
---
+++ Shop.Web/app/shared/services/authData.js
@@ -0,0 +1,15 @@
+﻿// xem usr có đăng nhập chưa
+(function (app) {
+    'use strict';
+    app.factory('authData', [function () {
+        var authDataFactory = {};
+
+        var authentication = {
+            IsAuthenticated: false,
+            userName: ""
+        };
+        authDataFactory.authenticationData = authentication;
+
+        return authDataFactory;
+    }]);
+})(angular.module('tedushop.common'));
